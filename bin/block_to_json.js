caebb29b08655f0a0dd3f12acb69d20a5c5bd540
Romain Beaumont <romain.rom1@gmail.com>
2015-10-20 18:53:43 +0200

    use a fixed date in item extractor (useful to get the data for a given minecraft version)

--- bin/block_to_json.js
+++ bin/block_to_json.js
@@ -1,14 +1,15 @@
-if(process.argv.length !=5) {
-  console.log("Usage : node block_to_json.js <blocks.json> <existingItems.json> <existingBlocks.json>");
+if(process.argv.length <5 || process.argv.length >6) {
+  console.log("Usage : node block_to_json.js <blocks.json> <existingItems.json> <existingBlocks.json> [<wikidate>]");
   process.exit(1);
 }
 var blockFilePath=process.argv[2];
 var existingItems=require(process.argv[3]);
 var existingBlocks=require(process.argv[4]);
+var date=process.argv[5] ? process.argv[5] : "2015-05-07T00:00:00Z";
 
 var writeAllBlocks=require("./../lib/block_extractor")(existingItems,existingBlocks).writeAllBlocks;
 
-writeAllBlocks(blockFilePath,function(err){
+writeAllBlocks(blockFilePath,date,function(err){
   if(err) {
     console.log(err.stack);
     return;
--- bin/item_to_json.js
+++ bin/item_to_json.js
@@ -1,12 +1,13 @@
-if(process.argv.length !=3) {
-  console.log("Usage : node item_to_json.js <items.json>");
+if(process.argv.length <3 || process.argv.length >4) {
+  console.log("Usage : node item_to_json.js <items.json> [<wikidate>]");
   process.exit(1);
 }
 var itemFilePath=process.argv[2];
+var date=process.argv[3] ? process.argv[3] : "2015-05-07T00:00:00Z";
 
 var writeAllItems=require("./../lib/item_extractor").writeAllItems;
 
-writeAllItems(itemFilePath,function(err){
+writeAllItems(itemFilePath,date,function(err){
   if(err) {
     console.log(err.stack);
     return;
