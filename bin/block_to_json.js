4e9b997ee87b46f1897267ccf0ecb1299492c805
Romain Beaumont <romain.rom1@gmail.com>
2016-05-23 17:23:05 +0200

    add mcpe extraction support : mainly an option for mcpe/mcpe , changing the url depending on that and some fixing

--- bin/block_to_json.js
+++ bin/block_to_json.js
@@ -1,5 +1,5 @@
-if(process.argv.length <5 || process.argv.length >6) {
-  console.log("Usage : node block_to_json.js <blocks.json> <existingItems.json> <existingBlocks.json> [<wikidate>]");
+if(process.argv.length <5 || process.argv.length >7) {
+  console.log("Usage : node block_to_json.js <blocks.json> <existingItems.json> <existingBlocks.json> [<wikidate>] [<mcpe|mcpe>]");
   process.exit(1);
 }
 var blockFilePath=process.argv[2];
@@ -7,10 +7,11 @@ var fs=require("fs");
 var existingItems=JSON.parse(fs.readFileSync(process.argv[3]));
 var existingBlocks=JSON.parse(fs.readFileSync(process.argv[4]));
 var date=process.argv[5] ? process.argv[5] : "2015-05-07T00:00:00Z";
+var mcpe=process.argv[6] ? process.argv[6]=="mcpe" : false;
 
 var writeAllBlocks=require("./../lib/block_extractor")(existingItems,existingBlocks).writeAllBlocks;
 
-writeAllBlocks(blockFilePath,date,function(err){
+writeAllBlocks(blockFilePath,date,mcpe,function(err){
   if(err) {
     console.log(err.stack);
     return;
--- bin/item_to_json.js
+++ bin/item_to_json.js
@@ -1,13 +1,14 @@
-if(process.argv.length <3 || process.argv.length >4) {
-  console.log("Usage : node item_to_json.js <items.json> [<wikidate>]");
+if(process.argv.length <3 || process.argv.length >5) {
+  console.log("Usage : node item_to_json.js <items.json> [<wikidate>] [<mcpe|mcpe>]");
   process.exit(1);
 }
 var itemFilePath=process.argv[2];
 var date=process.argv[3] ? process.argv[3] : "2015-05-07T00:00:00Z";
+var mcpe=process.argv[4] ? process.argv[4]=="mcpe" : false;
 
 var writeAllItems=require("./../lib/item_extractor").writeAllItems;
 
-writeAllItems(itemFilePath,date,function(err){
+writeAllItems(itemFilePath,date,mcpe,function(err){
   if(err) {
     console.log(err.stack);
     return;
