96291d41e29321a5eb0477e75c2d29141c2427f6
Romain Beaumont <romain.rom1@gmail.com>
2017-01-27 11:15:44 +0100

    add 1.11 data, fix recipe extractor (aliases lua file changed), fix enchantment extractor a bit and fix block extractor a bit

--- lib/common/toJSON.lua
+++ lib/common/toJSON.lua
@@ -1,5 +1,18 @@
+function getValue(v)
+   if (type(v) == "table" and v['name'] ~= nil) then
+        return v['name']
+   else
+        return v
+   end
+end
 local json='{'
 for k, v in pairs( aliases ) do
-   json=json..'"'..k..'"'..':'..'"'.. v..'"'..','
+    if (type(v) == "table" and v['name'] == nil ) then
+        for k2, v2 in pairs(v) do
+            json=json..'"'..k..'"'..':'..'"'.. getValue(v2)..'"'..','
+        end
+    else
+        json=json..'"'..k..'"'..':'..'"'.. getValue(v)..'"'..','
+    end
 end
 return json
