0bc44a6031670a3b8dc0130a17435399499767e1
Uncovery <uncovery@users.noreply.github.com>
2020-05-11 21:31:11 +0800

    fix default date to 1.15 safe date

--- bin/enchantments_to_json.js
+++ bin/enchantments_to_json.js
@@ -3,7 +3,7 @@ if(process.argv.length <3 || process.argv.length >4) {
   process.exit(1);
 }
 var enchantmentsFilePath=process.argv[2];
-var date=process.argv[3] ? process.argv[3] : "2015-07-11T00:00:00Z";
+var date=process.argv[3] ? process.argv[3] : "2019-12-31T00:00:00Z";
 
 var writeAllEnchantments=require("./../lib/enchantment_extractor").writeAllEnchantments;
 
